b4aab01ded2fbf3929f57d0d64138e9e53c845bb
ghostedxcx <complxent@gmail.com>
2022-04-16 07:13:42 +0100

    Create module and factory pattern to store gameBoard and players

--- script.js
+++ script.js
@@ -1,15 +1,29 @@
-// store gameboard as an array inside of an object
-var game = {
-    board: []
+
+
+// object to control the flow of the game
+var gameFlow = {
 }
 
-// store players inside of object for each player
-var player1 = {
-    name: "",
+// use a module to create a gameboard as an array inside of an object
+var moduleBoard = (function() {
+    var game = {
+        board: []
+    }
+
+    return({ game });
+
+})();
+
+// create a factory function to store players inside of object
+function playerFactory(name,) {
+    var player = {
+        name: name,
+    }
+    var player2 = {
+        name: name,
+    }
+    return player, player2;
 }
 
-var player2 = {
-    name: "",
-}
-
-
+console.log(moduleBoard.game.board);
+console.log(playerFactory('player1'));
